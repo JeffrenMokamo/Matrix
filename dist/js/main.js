35371317808a3883c1a7fa1be0e0e2a787064387
jeffren <jeffrenmokamo726@gmail.com>
2024-09-19 21:15:36 +0200

    page active

---
+++ dist/js/main.js
@@ -0,0 +1,19 @@
+const links = document.querySelectorAll('nav ul li a');
+
+function setActiveLink(){
+    const currentPath = document.location.pathname; 
+
+    links.forEach(link =>{
+        link.classList.remove('link-active');
+
+        const linkPath = "/" + link.getAttribute('href');
+
+        if(currentPath == linkPath){
+            link.classList.add('link-active');
+        }
+
+    })
+
+}
+
+window.addEventListener('load', setActiveLink());
