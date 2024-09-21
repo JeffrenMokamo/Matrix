dcebf598423a241fffb8e75f74f550fdbca8121f
jeffren <jeffrenmokamo726@gmail.com>
2024-09-21 12:25:00 +0200

    carousel

--- dist/js/main.js
+++ dist/js/main.js
@@ -27,3 +27,35 @@ setTimeout(() => {
     Preload.style.display = 'none';
 
 }, 3000);
+
+// Slide Accueil
+
+
+
+const carouSlide = document.querySelector('.slide');
+const widthImage = 100;
+const images = document.querySelectorAll('.slide-item');
+let ctr = 0;
+let ctrCloneImage = 0;
+
+function changeCarousel(){
+
+    carouSlide.style.transform = `translateX(-${widthImage * ctr}%)`;
+    ctr++;
+
+    if(ctr > images.length){
+        const cloneImage = images[ctrCloneImage].cloneNode(true);
+        carouSlide.appendChild(cloneImage);
+
+        ctrCloneImage++;
+        if(ctrCloneImage >= images.length){
+            ctrCloneImage = 0;
+        }
+    }
+}
+
+setInterval(() => {
+    changeCarousel();
+}, 5000);
+
+window.addEventListener('resize', changeCarousel());
